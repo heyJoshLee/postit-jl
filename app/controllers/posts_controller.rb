6b351d795e659e0a0c4373b8954a1b4783e74b9f
jleewebdev <lee45276@gmail.com>
2016-04-26 19:20:12 +0800

    Add create action and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your post has been saved"
+      redirect_to post_path(@post)
+    else
+      render "new"
+    end
+  end
+
+  def post_params
+    params.require(:post).permit!
+  end
 end
