7188f4e0583202bb334cefb158776f325a5c4970
jleewebdev <lee45276@gmail.com>
2016-04-27 12:41:15 +0800

    Add post form error showing add post#edit post#update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
+
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -14,14 +17,32 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      flash[:notice] = "Your post has been saved"
+      flash[:success] = "Your post has been saved"
       redirect_to post_path(@post)
     else
       render "new"
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Post successfully updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end 
+
   def post_params
     params.require(:post).permit!
   end
+
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
