23ff5fc9095c4c10b1fa52cf84fce4b0157342e9
jleewebdev <lee45276@gmail.com>
2016-04-27 19:49:12 +0800

    Add date and url helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_url(url)
+    url.starts_with?("http://") ? url : "http://" + url
+  end
+
+  def format_datetime(dt)
+    dt.strftime("%m/%d/%Y %I:%M%P")
+  end
 end
